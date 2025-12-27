15ce2d232decbddfe395525f85d81bc99e6bc1ef
lens0021 <lorentz0021@gmail.com>
2025-12-27 14:24:02 +0900

    Add import

--- app/src/main/java/io/github/lens0021/teogeul/ui/SettingsActivity.kt
+++ app/src/main/java/io/github/lens0021/teogeul/ui/SettingsActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
